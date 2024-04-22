a6bcb8cb08afcdd21fd633e0788dfc31e9155688
Ryan <mildenhallryan@gmail.com>
2024-04-22 16:08:32 +0200

    Created Local Library up until first half of Tutorial 6

--- django_test/locallibrary/catalog/views.py
+++ django_test/locallibrary/catalog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_books_word = Book.objects.filter(title__icontains='4').count()
+    num_genres_word = Genre.objects.filter(name__icontains='fiction').count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_books_word': num_books_word,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres_word': num_genres_word,  
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):   # Will query template at .../catalog/book_list.html
+    model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
